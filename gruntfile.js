b8f17741dafca24c774892221933d667a1f1c10f
Hailton Silva <hailton@hailton.com>
2014-09-07 11:05:17 +0100

    Updating tasks and adding new ones

--- gruntfile.js
+++ gruntfile.js
@@ -14,9 +14,9 @@ module.exports = function(grunt) {
         files: {
           "css/powertocss.css": "@dev/css/powertocss.less",
           "css/standard-theme.css": "@dev/css/standard-theme.less",
-          "starter-kit/css/powertocss.css": "@dev/css/powertocss.less",
-          "starter-kit/css/standard-theme.css": "@dev/css/standard-theme.less",
-          "starter-kit/css/your-theme.css": "@dev/css/your-theme.less",
+          "@dev/starter-kit/css/powertocss.css": "@dev/css/powertocss.less",
+          //"@dev/starter-kit/css/standard-theme.css": "@dev/css/standard-theme.less",
+          "@dev/starter-kit/css/your-theme.css": "@dev/css/your-theme.less",
         }
       },
       production: {
@@ -33,18 +33,18 @@ module.exports = function(grunt) {
         files: {
           "css/powertocss.min.css": "@dev/css/powertocss.less",
           "css/standard-theme.min.css": "@dev/css/standard-theme.less",
-          "starter-kit/css/powertocss.min.css": "@dev/css/powertocss.less",
-          "starter-kit/css/standard-theme.min.css": "@dev/css/standard-theme.less",
+          "@dev/starter-kit/css/powertocss.min.css": "@dev/css/powertocss.less",
+          //"@dev/starter-kit/css/standard-theme.min.css": "@dev/css/standard-theme.less",
         }
       }
     },
 
     // ===================================
-    // Harp Task
+    // Harp Task - run separatly with "grunt harp"
     // ===================================
     harp: {
       server: {
-        //server: true,
+        server: true, //Comment this to compile and uncomment to run the server
         source: '@dev'
       },
       dist: {
@@ -53,16 +53,44 @@ module.exports = function(grunt) {
       }
     },
 
+    // ==================================
+    // Copy Task
+    // will copy the less files to the Starter Kit
+    // ==================================
+    copy: {
+      main: {
+        expand: true, cwd: '@dev/css/', src: ['**'], dest: '@dev/starter-kit/less'
+      },
+    },
+
+    // =======================================
+    // compress Task
+    // =======================================
+    compress: {
+      main: {
+        options: {
+          archive: '@dev/starter-kit.zip'
+        },
+        files: [
+          //{src: ['starter-kit/*'], dest: 'internal_folder/', filter: 'isFile'}, // includes files in path
+          //{src: ['starter-kit/**'], dest: 'internal_folder2/'}, // includes files in path and its subdirs
+          //{expand: true, cwd: 'starter-kit/', src: ['**'], dest: 'internal_folder3/'}, // makes all src relative to cwd
+          {expand: true, cwd: '@dev/starter-kit/', src: ['**'], dest: './'}, // makes all src relative to cwd
+          //{flatten: true, src: ['@dev/starter-kit/**'], dest: '/', filter: 'isFile'} // flattens results to a single level
+        ]
+      }
+    },
+
     // ===================================
-    // Watch Task - run separatly without the server
+    // Watch Task
     // it will watch the LESS files
-    // command to run "grunt watch"
     // ===================================
     watch: {
         files: ("@dev/css/powertocss.less", "@dev/css/standard-theme.less", "@dev/css/your-theme.less"), // this is the less path
-        tasks: ["less"]
-    }
-
+        files: ("@dev/starter-kit/**"), // watch files to compress
+        files: ("@dev/css/**"), // watch files to copy
+        tasks: ["less", "compress", "copy"]
+    },
 
   });
 
@@ -70,6 +98,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-harp');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
 
-  grunt.registerTask('default', [ 'less', 'harp' ]);
+  grunt.registerTask('default', [ 'less', 'copy', 'compress', 'watch' ]);
 };
